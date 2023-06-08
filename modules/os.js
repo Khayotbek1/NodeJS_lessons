fd887f4da717996dd19ad49be7f7ab245c090f2b
C L O N E * <114242863+Khayotbek1@users.noreply.github.com>
2023-06-08 16:45:26 +0500

    module os

---
+++ modules/os.js
@@ -0,0 +1,19 @@
+const os = require('os')
+
+//PLatform
+// console.log(os.platform());
+
+//Information
+// console.log(os.cpus());
+
+//Architecture
+// console.log(os.arch());
+
+//freememory
+// console.log(os.freemem());
+
+//total memory
+// console.log(os.totalmem());
+
+//entr point
+// console.log(os.homedir());
