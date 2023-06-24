88d96d8d998e542e98f737781408d8b9e846ab23
C L O N E * <114242863+Khayotbek1@users.noreply.github.com>
2023-06-24 17:02:03 +0500

    array in object

--- app.js
+++ app.js
@@ -1,91 +1,105 @@
-const bill = (products, tax) => {
-    let total = 0
+// const bill = (products, tax) => {
+//     let total = 0
 
-    for (let i = 0; i < products.length; i++) {
-        total += products[i] + products[i] * tax
-    }
+//     for (let i = 0; i < products.length; i++) {
+//         total += products[i] + products[i] * tax
+//     }
 
-    return total
-}
+//     return total
+// }
 
-const result = bill([10, 15, 45], 0.2)
-console.log(result);
+// const result = bill([10, 15, 45], 0.2)
+// console.log(result);
 
-function calcAge(birthTear, firstName) {
-    const age = 2022 - birthTear
-    console.log(`${firstName} siz ${age} yoshdasiz`);
-    return age
-}
+// function calcAge(birthTear, firstName) {
+//     const age = 2022 - birthTear
+//     console.log(`${firstName} siz ${age} yoshdasiz`);
+//     return age
+// }
 
-const age = calcAge(1988, 'Hayot')
+// const age = calcAge(1988, 'Hayot')
 
-const names = ['hayot', 'yunus', 'yusuf', 'abror']
-const newNames = []
+// const names = ['hayot', 'yunus', 'yusuf', 'abror']
+// const newNames = []
 
-names.forEach((name) => {
-    const newName = name.charAt().toUpperCase() + name.slice(1).toLowerCase() + 'bek'
-    newNames.push(newName)
-})
+// names.forEach((name) => {
+//     const newName = name.charAt().toUpperCase() + name.slice(1).toLowerCase() + 'bek'
+//     newNames.push(newName)
+// })
 
-console.log(newNames);
+// console.log(newNames);
 
-function yih(n) {
-    return n * (n + 1) / 2
-}
-console.log(yih(10));
+// function yih(n) {
+//     return n * (n + 1) / 2
+// }
+// console.log(yih(10));
 
-//
-const cars = ['gentra', 'nexia', 'spark', 'damas']
-const newCars = []
+// forEach methods
+// const cars = ['gentra', 'nexia', 'spark', 'damas']
+// const newCars = []
 
-cars.forEach((name) => {
-    const newName = name.charAt().toUpperCase() + name.slice(1).toLowerCase() + ' uz';
-    newCars.push(newName)
-})
-console.log(newCars);
-
-//
-const students = ['Khayot', 'Yunus', 'Yusuf', 'Abror']
-const newStudents = []
-
-students.forEach((name) => {
-    const newName = name.charAt().toLowerCase() + name.slice(1).toUpperCase() + 'bek';
-    newStudents.push(newName);
-})
-console.log(newStudents);
-
+// cars.forEach((name) => {
+//     const newName = name.charAt().toUpperCase() + name.slice(1).toLowerCase() + ' uz';
+//     newCars.push(newName)
+// })
+// console.log(newCars);
 
 //
-const doctors = ['lor', 'cardio']
-const newDoctors = []
-doctors.forEach((name) => {
-    const newName = name.charAt().toUpperCase() + name.slice(1).toLowerCase() + ' doc'
-    newDoctors.push(newName)
+// const students = ['Khayot', 'Yunus', 'Yusuf', 'Abror']
+// const newStudents = []
+
+// students.forEach((name) => {
+//     const newName = name.charAt().toLowerCase() + name.slice(1).toUpperCase() + 'bek';
+//     newStudents.push(newName);
+// })
+// console.log(newStudents);
+
+
+//
+// const doctors = ['lor', 'cardio']
+// const newDoctors = []
+// doctors.forEach((name) => {
+//     const newName = name.charAt().toUpperCase() + name.slice(1).toLowerCase() + ' doc'
+//     newDoctors.push(newName)
+// })
+
+// console.log(newDoctors);
+
+// this kalit so'zi
+// const user = {
+//     name: 'Khayot',
+//     age: 25,
+//     email: 'hrazzoqov0002@gmail.com',
+//     location: 'Fergana',
+//     langs: ['uzbek', 'russian', 'english'],
+//     login: function () {
+//         console.log('Welcome to my site');
+//     },
+//     logout: function () {
+//         console.log('Bye bye');
+//     },
+//     speak: function () {
+//         console.log('I can speak');
+//         this.langs.forEach((lang) => {
+//             console.log(lang);
+//         })
+//     }
+
+// }
+
+// user.speak()
+
+
+const movies = [
+    { name: 'Avatar', likes: 456 },
+    { name: 'Harry Potter', likes: 951 },
+    { name: 'Titanic', likes: 678 },
+    { name: 'Sherlock', likes: 125 },
+    { name: 'Jobs', likes: 302 },
+]
+
+movies.forEach((movie) => {
+    const result = `Movie name: ${movie.name}, Movie like: ${movie.likes}`;
+    console.log(result);
 })
-
-console.log(newDoctors);
-
-const user = {
-    name: 'Khayot',
-    age: 25,
-    email: 'hrazzoqov0002@gmail.com',
-    location: 'Fergana',
-    langs: ['uzbek', 'russian', 'english'],
-    login: function () {
-        console.log('Welcome to my site');
-    },
-    logout: function () {
-        console.log('Bye bye');
-    },
-    speak: function () {
-        console.log('I can speak');
-        this.langs.forEach((lang) => {
-            console.log(lang);
-        })
-    }
-
-}
-
-user.speak()
-
 
